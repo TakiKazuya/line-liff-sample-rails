e07f9b2532283950621a97024d5bb983872fe71f
滝和弥 <kazup0689@gmail.com>
2022-12-07 15:15:17 +0900

    welcomeページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  root 'welcome#index'
   resources :reservations
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
